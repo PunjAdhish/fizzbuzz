f8b0a1bf4eab2caf9258b15e68aca6c7ac8855c2
jayeshpatel1218 <jy331399@dal.ca>
2019-05-23 09:35:19 -0300

    Continous code for task1

--- task1.py
+++ task1.py
@@ -1,3 +1,9 @@
-target_file=input("Book1 Book2 Book3....:")
-if target_file=="Book1":
-	fread=open("Book1.txt",'r')
+def input_option():
+	target_file=input("Book1 Book2 Book3....:")
+	if target_file=="Book1":
+		fread=open("Book1.txt",'r')
+	elif target_file=="Book2":
+		fread=open("Book2.txt",'r')
+	elif target_file=="Book3":
+		fread=open("Book3.txt",'r')
+
