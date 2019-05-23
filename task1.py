970422839d1af31cbd7defd01441a7c63fd87db7
Adhish Punj <ad543369@dal.ca>
2019-05-23 09:54:51 -0300

    trying to update

--- task1.py
+++ task1.py
@@ -1,9 +1,13 @@
 def input_option():
 	target_file=input("Book1 Book2 Book3....:")
+	check=0
 	if target_file=="Book1":
 		fread=open("Book1.txt",'r')
 	elif target_file=="Book2":
 		fread=open("Book2.txt",'r')
 	elif target_file=="Book3":
 		fread=open("Book3.txt",'r')
+		check=1
+
+
 
